50f536ac4220051550e50dbc95ff7b5a7cd52a1f
Benjamin Holmes <benjamin.holmes@gmail.com>
2017-11-08 12:11:21 +0000

    Refactored script

--- scripts/clean-slate.sh
+++ scripts/clean-slate.sh
@@ -2,14 +2,15 @@
 
 if [ -f /run/secrets/kubernetes.io/serviceaccount/token ]; then
 	TOKEN=`cat /run/secrets/kubernetes.io/serviceaccount/token`
+	CA=/run/secrets/kubernetes.io/serviceaccount/ca.crt
 else
 	echo "No token found. Are you running on OpenShift?"
 fi
 
 # Make sure we're logged in
-if [ -n "$TOKEN" g]; then
+if [ -n "$TOKEN" ]; then
 	echo "Authenticating with token"
-	oc login $KUBERNETES_SERVICE_HOST --token=$TOKEN --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt
+	oc login $KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT --token=$TOKEN --certificate-authority=$CA
 fi
 
 # Iterate through DCs, and execute a rollout on them
